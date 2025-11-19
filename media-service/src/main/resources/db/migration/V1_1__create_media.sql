2140770343678e6e002130dcc07e5ba7a5927e9c
Павлов Кирилл Григорьевич <kiryl.paulau@softclub.by>
2025-11-19 12:08:36 +0300

    Init media-service, refactor db

---
+++ media-service/src/main/resources/db/migration/V1_1__create_media.sql
@@ -0,0 +1,9 @@
+CREATE TABLE media (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    file_name VARCHAR(255) NOT NULL,
+    content_type VARCHAR(255) NOT NULL,
+    size BIGINT NOT NULL,
+    storage_key VARCHAR(255) NOT NULL, -- путь в S3 / MinIO
+    uploaded_at TIMESTAMP DEFAULT now()
+);
